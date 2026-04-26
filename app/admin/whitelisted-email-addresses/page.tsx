f1e5447cf36adac80cc78f45025e05cbb12eb193
onethousanddollarbeef <jw4280@barnard.edu>
2026-04-26 15:16:11 -0400

    Use caption_scores for top captions and align metric values

---
+++ app/admin/whitelisted-email-addresses/page.tsx
@@ -0,0 +1,8 @@
+import AdminCrudTable from "@/components/AdminCrudTable";
+
+type Props = { searchParams?: { page?: string } };
+
+export default function Page({ searchParams }: Props) {
+  const page = Number(searchParams?.page ?? "1") > 0 ? Number(searchParams?.page ?? "1") : 1;
+  return <AdminCrudTable title="Whitelisted E-mail Addresses (CRUD)" table="whitelist_email_addresses" path="/admin/whitelisted-email-addresses" page={page} />;
+}
